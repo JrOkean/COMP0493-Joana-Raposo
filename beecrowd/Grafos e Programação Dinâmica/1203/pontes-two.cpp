6a6add5826217cdd924f483b6f6d8dd64f3f23e5
JrOkean <amarelaxx@gmail.com>
2025-01-31 20:50:55 -0300

    changed:: pontes-two for manual allocation

--- beecrowd/Grafos e Programação Dinâmica/1203/pontes-two.cpp
+++ beecrowd/Grafos e Programação Dinâmica/1203/pontes-two.cpp
@@ -1,50 +1,65 @@
 #include <iostream>
-#include <vector>
-
 using namespace std;
 
-// K pontes (edges), R regions (vertex).
-// g(K) == 2 (porque liga EXATAMENTE duas regiões).
-
-bool subset_verification(int bridges_value, vector<int>& graus) {
-    vector<bool> dp( + 1, false);
+bool subset_verification(int bridges_value, int* graus, int regions) {
+    bool* dp = new bool[bridges_value + 1];
+    for (int i = 0; i <= bridges_value; ++i) {
+        dp[i] = false;
+    }
     dp[0] = true;
 
-    for (int grau : graus) {
-        for (int i = bridges_value; i >= grau; --i) {
-            if (dp[i - grau]) {
-                dp[i] = true;
+    for (int i = 1; i <= regions; ++i) {
+        for (int j = bridges_value; j >= graus[i]; --j) {
+            if (dp[j - graus[i]]) {
+                dp[j] = true;
             }
         }
     }
 
-    return dp[bridges_value];
+    bool result = dp[bridges_value];
+    delete[] dp;
+    return result;
 }
 
-int main(){
-    int bridges,  regions;
+int main() {
+    int bridges, regions;
 
-    while(cin >> regions >> bridges){
-        vector<vector<int>> matrix(regions + 1, vector<int>(regions + 1, 0));
-        for (size_t i = 0; i < bridges; i++){
+    while (cin >> regions >> bridges) {
+        int** matrix = new int*[regions + 1];
+        for (int i = 0; i <= regions; ++i) {
+            matrix[i] = new int[regions + 1];
+            for (int j = 0; j <= regions; ++j) {
+                matrix[i][j] = 0;
+            }
+        }
+
+        for (int i = 0; i < bridges; ++i) {
             int A, B;
             cin >> A >> B;
             matrix[A][B] += 1;
-            matrix[A][B] += 1;
+            matrix[B][A] += 1;
         }
 
-        vector<int> graus(regions+1, 0);
-        for (size_t i = 1; i <= regions; i++){
-            for (size_t j = 1; j <= regions; j++){
+        int* graus = new int[regions + 1];
+        for (int i = 1; i <= regions; ++i) {
+            graus[i] = 0;
+            for (int j = 1; j <= regions; ++j) {
                 graus[i] += matrix[i][j];
             }
         }
-        
-        if (subset_verification(bridges, graus)){
+
+        if (subset_verification(bridges, graus, regions)) {
             cout << "S" << endl;
-        } else{
+        } else {
             cout << "N" << endl;
         }
+
+        for (int i = 0; i <= regions; ++i) {
+            delete[] matrix[i];
+        }
+        delete[] matrix;
+        delete[] graus;
     }
+
     return 0;
 }
